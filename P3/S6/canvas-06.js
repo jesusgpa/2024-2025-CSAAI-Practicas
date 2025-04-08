c63e1d71bfcec68accd8637526396711266074ec
jesusgpa <jesus.parrado@urjc.es>
2025-04-08 09:05:27 +0200

    clase 2.4.2025

--- P3/S6/canvas-06.js
+++ P3/S6/canvas-06.js
@@ -15,6 +15,16 @@ ctx.fillStyle = 'yellow'
 ctx.fillText("Texto sólido", 10, 30);
 
 //-- Texto trazo
+console.log(ctx.lineWidth);
+ctx.lineWidth = 10;
 ctx.strokeStyle = 'red';
 ctx.font = "50px Arial";
 ctx.strokeText("Texto trazo", 5, 80);
+
+//-- Texto trazo 2
+//-- Cuando cambiamos una propiedad del contexto
+//-- Esa configuración sigue activa hasta que vuelve a cambiar
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'pink';
+ctx.font = "50px serif";
+ctx.strokeText("Hello world", 10, 130);
